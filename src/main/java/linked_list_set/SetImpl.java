ceee815090797eb985b7bdbd58663d5fb8b8db21
vitalya <Vitaliy.ovechkin.99@mail.ru>
2019-10-07 23:08:32 +0300

    Tests passed.

--- src/main/java/linked_list_set/SetImpl.java
+++ src/main/java/linked_list_set/SetImpl.java
@@ -1,18 +1,58 @@
 package linked_list_set;
 
+import kotlinx.atomicfu.AtomicRef;
+
 public class SetImpl implements Set {
+    private class AtomicReference {
+        private class NodeHolder {
+            final Node node;
+            final boolean mark;
+
+            NodeHolder(Node node, boolean initialMark) {
+                this.node = node;
+                this.mark = initialMark;
+            }
+        }
+
+        AtomicRef<NodeHolder> ref;
+
+        AtomicReference(Node node, boolean initialMark) {
+            this.ref = new AtomicRef<>(new NodeHolder(node, initialMark));
+        }
+
+        Node getReference() {
+            return ref.getValue().node;
+        }
+
+        boolean isRemoved() {
+            return ref.getValue().mark;
+        }
+
+        boolean compareAndSet(Node expectedReference, Node newReference, boolean expectedMark, boolean newMark) {
+            NodeHolder holder = ref.getValue();
+            return expectedReference == holder.node &&
+                    expectedMark == holder.mark &&
+                    ref.compareAndSet(holder, new NodeHolder(newReference, newMark));
+        }
+    }
+
     private class Node {
-        Node next;
+        AtomicReference next;
         int x;
 
         Node(int x, Node next) {
-            this.next = next;
+            this.next = new AtomicReference(next, false);
             this.x = x;
         }
     }
 
     private class Window {
         Node cur, next;
+
+        Window(Node cur, Node next) {
+            this.cur = cur;
+            this.next = next;
+        }
     }
 
     private final Node head = new Node(Integer.MIN_VALUE, new Node(Integer.MAX_VALUE, null));
@@ -21,46 +61,58 @@ public class SetImpl implements Set {
      * Returns the {@link Window}, where cur.x < x <= next.x
      */
     private Window findWindow(int x) {
-        Window w = new Window();
-        w.cur = head;
-        w.next = w.cur.next;
-        while (w.next.x < x) {
-            w.cur = w.next;
-            w.next = w.cur.next;
+        while (true) {
+            Node cur = head;
+            Node next = cur.next.getReference();
+            while (next.x < x) {
+                AtomicReference node = next.next;
+                if (node.isRemoved()) {
+                    if (!cur.next.compareAndSet(next, node.getReference(), false, false))
+                        break;
+                    next = node.getReference();
+                } else {
+                    cur = next;
+                    next = cur.next.getReference();
+                }
+            }
+            if (next.next.isRemoved()) {
+                cur.next.compareAndSet(next, next.next.getReference(), false, false);
+            } else {
+                return new Window(cur, next);
+            }
         }
-        return w;
+
     }
 
     @Override
     public boolean add(int x) {
-        Window w = findWindow(x);
-        boolean res;
-        if (w.next.x == x) {
-            res = false;
-        } else {
-            w.cur.next = new Node(x, w.next);
-            res = true;
+        while (true) {
+            Window w = findWindow(x);
+            if (w.next.x == x)
+                return false;
+            Node node = new Node(x, w.next);
+            if (w.cur.next.compareAndSet(w.next, node, false, false))
+                return true;
         }
-        return res;
     }
 
     @Override
     public boolean remove(int x) {
-        Window w = findWindow(x);
-        boolean res;
-        if (w.next.x != x) {
-            res = false;
-        } else {
-            w.cur.next = w.next.next;
-            res = true;
+        while (true) {
+            Window w = findWindow(x);
+            if (w.next.x != x)
+                return false;
+            AtomicReference nextNext = w.next.next;
+            if (w.next.next.compareAndSet(nextNext.getReference(), nextNext.getReference(), false, true)) {
+                w.cur.next.compareAndSet(w.next, nextNext.getReference(), false, false);
+                return true;
+            }
         }
-        return res;
     }
 
     @Override
     public boolean contains(int x) {
-        Window w = findWindow(x);
-        boolean res = w.next.x == x;
-        return res;
+        return findWindow(x).next.x == x;
     }
 }
+
